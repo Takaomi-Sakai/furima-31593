f930d83dc00500a05a9321bc2d8d01ad7adf2e04
Takaomi-Sakai <t.sakai0919.biz@gmail.com>
2021-02-22 21:10:10 +0900

    ユーザー管理機能　修正２回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+  validates :first_name
+  validates :last_name
+end
+with_options presence: true, format: { with: /[\p{katakana} ー－&&[^ -~｡-ﾟ]]+/, message: '全角カナを使用してください' } do
+  validates :first_name_ruby
+  validates :last_name_ruby
+end
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
   validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_ruby, presence: true
+  validates :first_name_ruby, presence: true
+  validates :birthday, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,22 +6,43 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+   context '保存できる場合' do
     it 'nicknameとemail、passwordとpassword_confirmation last_name, first_name, last_name_ruby, first_name_ruby, birthdayが存在すれば登録できる' do
       expect(@user).to be_valid
     end
-
+    
     it 'passwordとpassword_confirmationが6文字以上、半角英数字混合、パスワードとパスワード（確認用）、値の一致であれば登録できる' do
       @user.password = 'Aa1234'
       @user.password_confirmation = 'Aa1234'
       expect(@user).to be_valid
     end
 
-    it 'ユーザー本名のフリガナはカタカナであれば登録できる' do
+    it 'ユーザー本名は全角（漢字）であれば登録できる' do
+      @user.last_name = '酒井'
+      @user.first_name = '隆臣'
+      expect(@user).to be_valid
+    end
+
+    it 'ユーザー本名は全角（ひらがな）であれば登録できる' do
+      @user.last_name = 'さかい'
+      @user.first_name = 'たかおみ'
+      expect(@user).to be_valid
+    end
+
+    it 'ユーザー本名は全角（カタカナ）であれば登録できる' do
+      @user.last_name = 'サカイ'
+      @user.first_name = 'タカオミ'
+      expect(@user).to be_valid
+    end
+
+    it 'ユーザー本名のフリガナは全角（カタカナ）であれば登録できる' do
       @user.last_name_ruby = 'カタカナ'
       @user.first_name_ruby = 'カタカナ'
       expect(@user).to be_valid
     end
+  end
 
+  context '保存できない場合' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -66,4 +87,5 @@ RSpec.describe User, type: :model do
       expect(another_user.errors.full_messages).to include('Email has already been taken')
     end
   end
+ end
 end
