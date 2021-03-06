789a92e30957fc194241d83db366dfdeb937d129
Takaomi-Sakai <t.sakai0919.biz@gmail.com>
2021-03-06 19:31:11 +0900

    商品情報編集機能_修正１回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  # before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :contributor_confirmation, only: [:edit, :update]
   # bofore_action :move_to_index, except: [:index, :show]
 
   def index
@@ -21,17 +22,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path(@item)
+      redirect_to root_path(item)
     else
       render :edit
     end
@@ -43,4 +41,13 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:product_name, :discription, :price, :category_id, :status_id, :delivery_fee_id,
                                  :prefecture_id, :day_id, :image).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @item.user
+  end
+
 end
